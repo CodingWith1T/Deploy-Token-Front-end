33a8bcb90d1a4142db1f0a92ae8b669f34208ba3
CodingWith1T <abhishek.raghav1t@gmail.com>
2025-03-26 11:34:41 +0530

    Changes in Components

--- app/create-lock/page.js
+++ app/create-lock/page.js
@@ -10,40 +10,7 @@ import { VerifyToken } from '../../Componests/VerifyToken/VerifyToken';
 import Link from "next/link";
 import { waitForTransactionReceipt } from "@wagmi/core";
 import { ConnectButton } from '@rainbow-me/rainbowkit';
-
-const FormButton = ({ onClick, href, disabled, buttonName }) => {
-    const { isConnected } = useAccount();
-
-    const getButtonStyle = () => {
-        if (buttonName === 'Approve') return 'bg-red-500 hover:bg-red-600';
-        if (buttonName === 'Previous') return 'bg-gray-500 hover:bg-gray-600';
-        return 'bg-blue-500 hover:bg-blue-600';
-    };
-
-    return isConnected ? (
-        href ? (
-            
-            <a
-                href={href}
-                className={`clbtn inline-block px-4 py-2 text-white font-medium rounded-md ${getButtonStyle()} ${disabled ? 'opacity-50 cursor-not-allowed' : ''}`}
-            >
-                {buttonName}
-            </a>
-        ) : (
-            <div className="lbtnbox">
-            <button
-                onClick={onClick}
-                disabled={disabled}
-                className={`clbtn px-4 py-2 text-white font-medium rounded-md ${getButtonStyle()} ${disabled ? 'opacity-50 cursor-not-allowed' : ''}`}
-            >
-                {buttonName}
-            </button>
-            </div>
-        )
-    ) : (
-        <ConnectButton />
-    );
-};
+import FormButton from "../../Componests/FormButton/FormButton";
 
 const locker = {
     '11155111': '0x7AF39aC33c2a873E81A754c4B292B8a537bBeDc4',
